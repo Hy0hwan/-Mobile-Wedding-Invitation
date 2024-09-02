62bfcc7a96f5484b036ed1a0c3e016f288ab54a1
김효환 <gyghks1017@gmail.com>
2024-09-02 17:14:06 +0900

    [style] : 모바일 비율 수정

--- wedding-invitation/src/App.js
+++ wedding-invitation/src/App.js
@@ -19,14 +19,15 @@ const Container = styled.div`
   flex-direction: column; /* 세로 정렬 */
   justify-content: center;
   align-items: center;
-  /* background-color: #f0f0f0; */
-  max-width: 780px;
+  background-color: #f0f0f0;
+  max-width: 100vw; /* 화면 너비를 넘지 않도록 설정 */
   width: 100%; /* 화면에 맞춰 유연하게 조정 */
-  margin: 0 auto; /* 데스크탑에서 가운데 정렬 */
+  overflow-x: hidden; /* 가로 스크롤 방지 */
   padding: 20px; /* 내용물과 컨테이너 사이 여백 추가 */
+  box-sizing: border-box; /* 패딩을 포함하여 너비와 높이를 설정 */
 
   @media (max-width: 768px) {
-    padding: 10px; /* 모바일에서 여백을 조금 줄임 */
+    padding: 0px; /* 모바일에서 여백을 조금 줄임 */
     height: auto;
   }
 `;
