7af7ffd2c58d97b82d6bb836e96ff9a8d7d65acc
김효환 <gyghks1017@gmail.com>
2024-09-02 15:56:58 +0900

    [Feat] : 메인이미지 컴포넌트 생성 및 전체포지션 설정

--- wedding-invitation/src/App.js
+++ wedding-invitation/src/App.js
@@ -1,11 +1,28 @@
-import react from 'react';
+import React from 'react';
+import styled from 'styled-components';
+import MainImage from './components/MainImage';
 
 function App() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Container>
+      <MainImage />
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f0f0f0;
+  max-width: 780px;
+  width: 100%; /* 화면에 맞춰 유연하게 조정 */
+  margin: 0 auto; /* 데스크탑에서 가운데 정렬 */
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    height: auto;
+  }
+`;
---
+++ wedding-invitation/src/components/MainImage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import mainImage from '../assets/main.jpeg';
+
+const MainImage = () => {
+  return (
+    <ImageContainer>
+      <StyledImage src={mainImage} alt="메인이미지" />
+    </ImageContainer>
+  );
+};
+
+export default MainImage;
+
+// 이미지 컨테이너 스타일
+const ImageContainer = styled.div`
+  width: 100%;
+  overflow: hidden; // 이미지가 컨테이너를 넘어가지 않도록 설정
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+// 이미지 스타일
+const StyledImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover; // 이미지를 컨테이너에 맞게 확장하고 비율 유지
+  border-radius: 0; // 둥근 모서리를 없앰
+`;
