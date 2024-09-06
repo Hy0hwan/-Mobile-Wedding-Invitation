57e35807d749a8d2e330f36ec682e88f23592e02
김효환 <gyghks1017@gmail.com>
2024-09-06 16:01:48 +0900

    [Feat] : ImgBox 추가

--- wedding-invitation/src/App.js
+++ wedding-invitation/src/App.js
@@ -4,6 +4,7 @@ import MainImage from './components/MainImage';
 import Paragraph from './components/Paragraph';
 import SaveTheDate from './components/SaveTheDate';
 import Information from './components/Information';
+import ImgBox from './components/ImgBox';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Paragraph />
       <SaveTheDate />
       <Information />
+      <ImgBox />
     </Container>
   );
 }
@@ -24,10 +26,11 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   /* background-color: #f0f0f0; */
-  max-width: 780px;
+  max-width: 768px;
   width: 100%; /* 화면에 맞춰 유연하게 조정 */
   margin: 0 auto; /* 데스크탑에서 가운데 정렬 */
   padding: 20px; /* 내용물과 컨테이너 사이 여백 추가 */
+  gap: 10px;
 
   @media (max-width: 768px) {
     padding: 10px; /* 모바일에서 여백을 조금 줄임 */
---
+++ wedding-invitation/src/components/ImgBox.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import styled from 'styled-components';
+import img1 from '../assets/imgs/1.jpeg';
+import img2 from '../assets/imgs/2.jpeg';
+import img3 from '../assets/imgs/3.jpeg';
+import img4 from '../assets/imgs/4.png';
+import img5 from '../assets/imgs/5.jpeg';
+import img6 from '../assets/imgs/6.jpeg';
+import img7 from '../assets/imgs/7.jpeg';
+import img8 from '../assets/imgs/8.jpeg';
+import img9 from '../assets/imgs/9.jpeg';
+import img10 from '../assets/imgs/10.jpeg';
+
+const ImgBox = () => {
+  const images = [
+    {
+      original: img1,
+      thumbnail: img1,
+    },
+    {
+      original: img2,
+      thumbnail: img2,
+    },
+    {
+      original: img3,
+      thumbnail: img3,
+    },
+    {
+      original: img4,
+      thumbnail: img4,
+    },
+    {
+      original: img5,
+      thumbnail: img5,
+    },
+    {
+      original: img6,
+      thumbnail: img6,
+    },
+    {
+      original: img7,
+      thumbnail: img7,
+    },
+    {
+      original: img8,
+      thumbnail: img8,
+    },
+    {
+      original: img9,
+      thumbnail: img9,
+    },
+    {
+      original: img10,
+      thumbnail: img10,
+    },
+  ];
+
+  return (
+    <Container>
+      <ImageGallery items={images} />;
+    </Container>
+  );
+};
+
+export default ImgBox;
+
+const Container = styled.div`
+  width: 90%;
+`;
