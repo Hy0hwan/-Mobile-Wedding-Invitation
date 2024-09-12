04702a10112ca5f97378d93078e737c1b1330e3a
김효환 <gyghks1017@gmail.com>
2024-09-12 16:51:55 +0900

    [Design] : Paragraph 리뉴얼

--- wedding-invitation/src/components/Paragraph.jsx
+++ wedding-invitation/src/components/Paragraph.jsx
@@ -5,21 +5,32 @@ import paragraphImage from '../assets/paragraph.jpeg';
 const Paragraph = () => {
   return (
     <StyledContainer>
-      <StyledTextHead>우리, 결혼합니다.</StyledTextHead>
+      <StyledTextHead>이승원</StyledTextHead>
+      <StyledTextHead>임소정</StyledTextHead>
+      <StyledTextHead>결혼합니다 !</StyledTextHead>
+
+      <StyledInfoRow>
+        <StyledText>info</StyledText>
+        <StyledLine />
+      </StyledInfoRow>
+
+      {/* 왼쪽 정렬할 텍스트 */}
+      <StyledLeftText>2024년 11월 16일, 토요일 오후 5시 20분</StyledLeftText>
+      <StyledLeftText>그레이스파티 (신림) 7층</StyledLeftText>
+
       <StyledImage src={paragraphImage} alt="" />
-      <StyledText>그냥 좋은 것이</StyledText>
-      <StyledText>가장 좋은 것입니다.</StyledText>
-      <StyledText>그 때문에 그가 좋은 것이 아니라</StyledText>
-      <StyledText>그가 좋아 그 부분이 좋은 것입니다.</StyledText>
-      <StyledText>그냥 좋은 것이</StyledText>
-      <StyledText>그저 좋은 것입니다.</StyledText>
-      <StyledText>
-        원태연, <Highlighted>&lt;그냥 좋은 것&gt;</Highlighted>
-      </StyledText>
+      <StyledText>운명 같은 우리의 만남이</StyledText>
+      <StyledText>사랑의 결실을 맺어 평생 서로의</StyledText>
+      <StyledText>울타리가 되어 주고자 합니다</StyledText>
+      <StyledText>저희 두 사람이 하나 되는</StyledText>
+      <StyledText>그 시작의 자리에 함께</StyledText>
+      <StyledText>축복해 주시길 소망합니다.</StyledText>
+
       <StyledText>--</StyledText>
-      <StyledText>이희용의 아들 이승원</StyledText>
+
+      <StyledText>이희용의 아들 승원</StyledText>
       <StyledText>
-        이경희의 <HiddenLetter>ㄱ</HiddenLetter>딸 임소정
+        이경희의 <HiddenLetter>ㄱ</HiddenLetter>딸 소정
       </StyledText>
     </StyledContainer>
   );
@@ -39,32 +50,57 @@ const StyledContainer = styled.div`
 
 const StyledImage = styled.img`
   width: 100%;
-  height: 70%;
+  height: 350px;
 `;
 
 const StyledTextHead = styled.p`
-  font-size: 24px;
+  font-size: 30px;
   line-height: 1.6;
-  margin: 10px 0;
-  text-align: center; /* 각 줄을 가운데 정렬 */
+  margin: 0;
+  text-align: left; /* 왼쪽 정렬 */
   color: #333;
+  width: 100%; /* 부모 컨테이너 너비에 맞추기 */
+  padding-left: 20px; /* 왼쪽에 여백 추가 (선택 사항) */
 `;
-// 텍스트 스타일
+
+const StyledInfoRow = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding-left: 20px; /* info 줄이 왼쪽 여백에 맞춰지도록 조정 */
+`;
+
 const StyledText = styled.p`
-  font-size: 18px;
+  font-size: 20px;
   line-height: 1.6;
   margin: 10px 0;
-  text-align: center; /* 각 줄을 가운데 정렬 */
+  text-align: center; /* 기본 텍스트는 가운데 정렬 */
+  color: #333;
+  margin-right: 10px;
+`;
+
+// 왼쪽 정렬할 텍스트 스타일
+const StyledLeftText = styled.p`
+  font-size: 20px;
+  line-height: 1.6;
+  margin: 10px 0;
+  text-align: left; /* 왼쪽 정렬 */
+  width: 100%;
+  padding-left: 20px; /* 왼쪽 여백 추가 */
   color: #333;
 `;
 
-// 강조된 텍스트 스타일
+const StyledLine = styled.div`
+  flex-grow: 0.9;
+  height: 1px;
+  background-color: #333;
+`;
+
 const Highlighted = styled.span`
-  color: #e74c3c; /* 강조 색상 */
+  color: #e74c3c;
   font-weight: bold;
 `;
 
-// 숨김 문자 스타일
 const HiddenLetter = styled.span`
   visibility: hidden;
 `;
