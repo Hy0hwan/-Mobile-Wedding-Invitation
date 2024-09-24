8b55d7a4f2a24528c1c5140f9914728ddde5d284
김효환 <gyghks1017@gmail.com>
2024-09-25 00:24:23 +0900

    배포테스트

--- wedding-invitation/src/components/WeddingImg.jsx
+++ wedding-invitation/src/components/WeddingImg.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 import styled from 'styled-components';
 
 import img1 from '../assets/imgs/1.jpeg';
@@ -12,15 +14,26 @@ import img9 from '../assets/imgs/9.jpeg';
 import img10 from '../assets/imgs/10.jpeg';
 
 const WeddingImg = () => {
-  const images = [img1, img5, img7, img2, img6, img8, img9, img10, img4];
+  // Create an array of objects for the ImageGallery component
+  const images = [
+    { original: img1, thumbnail: img1 },
+    { original: img5, thumbnail: img5 },
+    { original: img7, thumbnail: img7 },
+    { original: img2, thumbnail: img2 },
+    { original: img6, thumbnail: img6 },
+    { original: img8, thumbnail: img8 },
+    { original: img9, thumbnail: img9 },
+    { original: img10, thumbnail: img10 },
+    { original: img4, thumbnail: img4 },
+  ];
 
   return (
     <GalleryContainer>
-      {images.map((src, index) => (
-        <GalleryItem>
-          <img src={src} alt={`Gallery item ${index + 1}`} />
-        </GalleryItem>
-      ))}
+      <ImageGallery
+        items={images}
+        showThumbnails={true}
+        showFullscreenButton={true}
+      />
     </GalleryContainer>
   );
 };
@@ -28,38 +41,14 @@ const WeddingImg = () => {
 export default WeddingImg;
 
 const GalleryContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
   padding: 10px;
   width: 100%;
   max-width: 100vw;
   box-sizing: border-box;
   margin: 0 auto;
 
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(2, 1fr); // 2열로 조정
-  }
-
-  @media (max-width: 480px) {
-    grid-template-columns: 1fr; // 1열로 조정
-  }
-`;
-
-const GalleryItem = styled.div`
-  position: relative;
-  overflow: hidden;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  aspect-ratio: 1 / 1;
-  box-sizing: border-box;
-  padding: 0;
-
-  img {
-    width: 100%;
-    height: 100%;
+  .image-gallery-slide img {
+    border-radius: 8px;
     object-fit: cover;
-    display: block;
-    box-sizing: border-box;
   }
 `;
